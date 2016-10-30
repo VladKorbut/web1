245f382f3ef9e2962ae6bfe4a179e4f1cad02a1f
Vladislav Korbut <v.korbut8@gmail.com>
2016-10-30 19:16:42 +0300

    handlebars was included

--- js/index.js
+++ js/index.js
@@ -18,17 +18,11 @@ function sendLink(){
 		type: 'POST',
 		dataType: 'jsonp',
 		success: function(data) {
-			if(!data.errorCode){
-				deleteElement('p','alert-danger');
-				deleteElement('input', 'short-url');
-				deleteElement('a', 'btn-clipboard');
-				var results = data.results[url.toString()].shortUrl;
-				postInput(results);
-				postButton();
+			if(!data.errorCode){	
+				var source   = $("#template").html();
+				var template = Handlebars.compile(source);
+				$('.shorten-url').html(template(data.results[url.toString()]));
 			}else{
-				deleteElement('p','alert-danger');
-				deleteElement('input', 'short-url');
-				deleteElement('a', 'btn-clipboard');
 				postError();
 			}
 		}
@@ -36,29 +30,10 @@ function sendLink(){
 
 	});
 }
-function deleteElement(tag, className){
-	if(document.getElementsByClassName(className)){
-		$(tag+"."+className).remove();
-	}
-}
-function postInput(results) {
-	var link = document.createElement('input');
-	link.setAttribute('value', results);
-	link.setAttribute('class', 'short-url form-control input-lg');
-	container.appendChild(link);
-}
-function postButton(){
-	var copy = document.createElement('a');
-	copy.setAttribute('onclick', 'copyText()');
-	copy.setAttribute('data-clipboard-target', '.short-url');
-	copy.setAttribute('class', 'btn-clipboard');
-	copy.innerHTML = "Скопировать ссылку";
-	container.appendChild(copy);
-}
 
 function postError(){
 	var error= document.createElement('p');
 	error.className = 'alert alert-danger';
 	error.innerHTML = "Неправильный ввод! Повторите попытку";
-	container.appendChild(error);
+	$('.shorten-url').html(error);
 }
